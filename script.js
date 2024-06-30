1eed373954ef9dee43acfac5884f6fc2bb633425
GarimaAvasthi <garimaavasthi06@gmail.com>
2024-06-30 14:43:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".input", {
+    strings: ["Beautiful Places To Visit,", "Select Your Destination Now,", "Beautiful Places To Visit,,", "Select Your Destination Now"],
+    typeSpeed:100,
+    backSpeed: 100,
+    loop: true
+});
